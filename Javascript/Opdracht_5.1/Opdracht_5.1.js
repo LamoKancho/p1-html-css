26a9786c2ad86bcc789cd0918b4230be8bb5847b
LamoKancho <9014772@student.zadkine.nl>
2022-11-03 14:47:35 +0100

    java 5.1 gemaakt met bonus

--- Javascript/Opdracht_5.1/Opdracht_5.1.js
+++ Javascript/Opdracht_5.1/Opdracht_5.1.js
@@ -1,2 +1,7 @@
-Function 
-document.getElementById("Title").style.color = "red";
+function maakrood(){
+    document.getElementById("Title").style.color = "red";
+    document.getElementById("Title").innerText = "hoi maar rood";
+    }
+
+    // 1. VEEL TELAAT INGELEVERD SORRY
+    // 2. ID word gebruikt zodat het een unieke code is en het niet zal mengen met andere codes.
